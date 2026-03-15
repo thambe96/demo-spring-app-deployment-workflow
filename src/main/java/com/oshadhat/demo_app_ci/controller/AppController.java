4b15d04d5f2680c27b341a56df7dd7c5e8b6da32
Oshadha Thambavita <osthambe20@gmail.com>
2026-03-15 22:07:06 +0530

    change message controller response

--- src/main/java/com/oshadhat/demo_app_ci/controller/AppController.java
+++ src/main/java/com/oshadhat/demo_app_ci/controller/AppController.java
@@ -11,7 +11,7 @@ public class AppController {
 
     @GetMapping("message")
     public ResponseEntity<String> getMessage() {
-        return ResponseEntity.ok("Hey there it's Oshadhat !! Spring Application is up and Running!");
+        return ResponseEntity.ok("Hey there it's Oshadhat.. !! Spring Application is up and Running!");
     }
 
 
